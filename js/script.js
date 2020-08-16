d06673d84cba4e73193a9dcf18eed19696dd73b5
Evheniy Kalinichenko <kalinichenko0997z@gmail.com>
2020-08-16 14:28:24 +0300

    Вывел сообщение закончились попытки

--- js/script.js
+++ js/script.js
@@ -1,30 +1,43 @@
 function isNum(number) {
-    return (!isNaN(parseFloat(number)) && isFinite(number));
+  return !isNaN(parseFloat(number)) && isFinite(number);
 }
 let game = function () {
-    const randomNum = parseInt(Math.random() * 100);
-    return function repeat() {
-        let userGuess = prompt("Угадай число от 1 до 100");
-        if (isNum(userGuess)) {
-            if (userGuess > randomNum) {
-                alert('Загаданное число меньше');
-                return repeat();
-            } else if (userGuess < randomNum) {
-                alert('Загаданное число больше');
-                return repeat();
-            } else {
-                if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
-                    game()();
-                } else {
-                    alert('Вы завершили игру');
-                }
-            }
-        } else if (userGuess === null) {
-            alert('Пока!')
+  const randomNum = parseInt(Math.random() * 100);
+  let count = 10;
+
+  return function repeat() {
+    count -= 1;
+    let userGuess = prompt("Угадай число от 1 до 100");
+    if (isNum(userGuess)) {
+      if (count > 0) {
+        if (userGuess > randomNum) {
+          alert("Загаданное число меньше. Осталось попыток: " + count);
+
+          return repeat();
+        } else if (userGuess < randomNum) {
+          alert("Загаданное число большею  Осталось попыток: " + count);
+
+          return repeat();
         } else {
-            return repeat();
+          if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+            game()();
+          } else {
+            alert("Вы завершили игру");
+          }
         }
+      } else {
+        if (confirm("Вы проиграли. Хотите сыграть еще раз?")) {
+          game()();
+        } else {
+          alert("Bye");
+        }
+      }
+    } else if (userGuess === null) {
+      alert("Пока!");
+    } else {
+      return repeat();
     }
-}
+  };
+};
 game()();
-console.dir(game())
+console.dir(game());
